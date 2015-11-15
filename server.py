aed40f3b1fffe97938f899be28fdb9dd32e86297
Pradeep Vairamani <pradeeprv123@gmail.com>
2015-11-15 17:21:46 -0500

    Changes to APIs

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def hello():
 
 @route('/search/<author_name>')
 def search_by_author_name(author_name):
-    return author_name
+    return {"author_name": "Noam Chomsky", "author_id": 25}
 
 
 @route('/details/<author_id>')
@@ -31,6 +31,6 @@ def get_author_details(author_id):
 
 @route('/similar/<author_id>')
 def get_similar_authors(author_id):
-    return author_id
+    return [{"id": 1, "name": "name1"}, {"id": 2, "name": "name2"}, {"id": 3, "name": "name3"}, {"id": 4, "name": "name4"}, {"id": 5, "name": "name5"}, {"id": 6, "name": "name6"}]
 
 run(host='localhost', port=8082, debug=True)
